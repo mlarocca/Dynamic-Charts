00893f055d504cd675e6fb03d168ef72953f61e2
Marcello La Rocca <marcellolarocca@gmail.com>
2012-12-22 20:41:09 +0100

    Docs updated

--- api.js
+++ api.js
@@ -19,12 +19,12 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "ChartUtils",
             "name": "ChartUtils",
-            "description": "Module ChartUtils\n        \nExtends Object class with several useful methods to allow better encapsulation mechanisms.\nExposes a few utility functions"
+            "description": "Module ChartUtils\n        \nExtends Object class with several useful methods to allow better encapsulation mechanisms.<br>\nExposes a few utility functions"
         },
         {
             "displayName": "DynamicChart",
             "name": "DynamicChart",
-            "description": "Module DynamicChart\n        \nThis module requires:\n<ol>\n    <li>{{#crossLinkModule \"chart_utils.js\"}}{{/crossLinkModule}}</li>\n    <li>{{#crossLink \"http://d3js.org/d3.v2.js\"}}{{/crossLink}}</li>\n</ol>\n\nExposes methods for creating different types of dynamic charts:\n<ol>\n    <li>BasicBarChart</li>\n    <li>FixedWidthBarChart</li>\n    <li>SlidingBarChart</li>\n    <li>TimeWheelChart</li>"
+            "description": "Module DynamicChart\n        \nThis module requires:\n<ol>\n    <li>{{#crossLinkModule \"chart_utils.js\"}}{{/crossLinkModule}}</li>\n    <li>{{#crossLink \"http://d3js.org/d3.v2.js\"}}{{/crossLink}}</li>\n</ol>\n<br>\nExposes methods for creating different types of dynamic charts:\n<ul>\n    <li>BasicBarChart</li>\n    <li>FixedWidthBarChart</li>\n    <li>SlidingBarChart</li>\n    <li>TimeWheelChart</li>\n</ul>"
         }
     ]
 } };
